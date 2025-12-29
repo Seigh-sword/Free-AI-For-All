0ae4450720b47a62becd324696518b8d17192c7d
Arunkumar .s.j. <zack.yt.7085@gmail.com>
2025-12-29 22:29:19 +0530

    Update AIHandler.js

--- AIHandler/AIHandler.js
+++ AIHandler/AIHandler.js
@@ -1,20 +1,21 @@
-import { AISyntax } from './AISyntax.js';
+// AIHandler/AIHandler.js
 import { AILoader } from './AILoader.js';
+import { AISyntax } from './AISyntax.js';
 
 export class AIHandler {
     constructor() {
         this.loader = new AILoader();
-        // Pass 'this' so Syntax can call handler.generate()
         this.syntax = new AISyntax(this); 
-        
+
+        // Expose all syntax methods
         this.txt = this.syntax.txt;
-        this.img = this.syntax.img;
+        this.img = this.syntax.img; // New: Image generation entry point
         this.set = this.syntax.set;
-        this.models = this.syntax.models;
+        this.models = this.syntax.models; // Expose available models
     }
 
     async generate() {
-        // AIHandler calls the loader using the syntax config
+        // AIHandler now passes the full config (including 'type') to the loader
         return await this.loader.load(this.syntax.config);
     }
 }
